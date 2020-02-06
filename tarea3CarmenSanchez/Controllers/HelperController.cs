4f893fbd74f3d7a26c3a20c7da91a6bb93b4d0e8
Carmen Sanchez <carmenmasato@gmail.com>
2020-02-05 22:21:04 -0400

    cambios

---
+++ tarea3CarmenSanchez/Controllers/HelperController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using tarea3CarmenSanchez.Models;
+
+namespace tarea3CarmenSanchez.Controllers
+{
+    public class HelperController : Controller
+    {
+        // GET: Helper
+        public ActionResult Formulario()
+        {
+            return View();
+        
+        }
+
+
+        [HttpPost]
+        public ActionResult Datos(Persona per, HttpPostedFileBase foto)
+        {
+            if (ModelState.IsValid)
+            {
+                string nombre = foto.FileName;
+                foto.SaveAs(Server.MapPath("/img/" + nombre));
+                ViewBag.picture = nombre;
+
+                return View(per);
+
+            }
+            else
+            {
+                return View("Formulario");
+            }
+
+        }
+    }
+}
